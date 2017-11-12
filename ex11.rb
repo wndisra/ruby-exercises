ff62f0dd791a42434abe3519c100c66794b108db
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-11-12 23:52:40 +0700

    Add 11th exercise

---
+++ ex11.rb
@@ -0,0 +1,8 @@
+print "How old are you? "
+age = gets.chomp # Get user input & remove line break (\n) at the end
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
