e9e21239922c16d581909179ce190c54f8e89e2b
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-11-12 22:46:37 +0700

    Add 5th exercise

---
+++ ex5.rb
@@ -0,0 +1,21 @@
+name = "Wanda Ichsanul Isra"
+age = 24
+height = 68
+weight = 125
+eyes = 'black'
+teeth = 'white'
+hair = 'black'
+
+cm = 2.54
+kg = 0.45
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{height * cm} cm tall."
+puts "He's #{weight} pounds heavy."
+puts "He's #{weight * kg} kg heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height}, and #{weight}, I get #{age + height + weight}"
