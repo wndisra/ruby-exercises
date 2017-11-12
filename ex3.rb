caa1940937530a4b4550822e01082cebdee642e1
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-11-12 21:15:26 +0700

    Add 3rd exercise

---
+++ ex3.rb
@@ -0,0 +1,19 @@
+puts "I will now count my chickens:"
+puts "Hens #{25 + 30 / 6.0}" # Use floating point number for accurate result
+puts "Roosters #{100 - 25 * 3.0 % 4}"
+
+puts "\nNow, I will count the eggs:"
+puts 3 + 2 + 1.0 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "\nIs it true that 3 + 2 < 5 - 7?"
+puts 3 + 2 < 5 - 7
+
+puts "\nWhat is 3 + 2? #{3 + 2.0}"
+puts "What is 5 - 7? #{5.0 - 7}"
+puts "Oh, that's why it's false."
+
+puts "\nHow about some more."
+
+puts "(5 > -2) - Is it greater? #{5 > -2}"
+puts "(5 >= -2) - Is it greater or equal? #{5 >= -2}"
+puts "(5 <= -2) - Is it less or equal? #{5 <= -2}"
