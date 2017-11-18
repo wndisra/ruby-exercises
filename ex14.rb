b11964af86f68d860172b99b6d97181fccdeaad2
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-11-19 01:34:26 +0700

    Add ex14 exercise

---
+++ ex14.rb
@@ -0,0 +1,24 @@
+user_name = ARGV.first
+prompt = '> '
+
+puts "Hi #{user_name}."
+puts "I'd like to ask you a few questions."
+puts "Do you like me #{user_name}? "
+print prompt
+likes = $stdin.gets.chomp
+
+puts "Where do you live #{user_name}? "
+print prompt
+lives = $stdin.gets.chomp
+
+# Use comma to puts two different things
+# puts "What kind of computer do you have? ", prompt
+puts "What kind of computer do you have? "
+print prompt
+computer = $stdin.gets.chomp
+
+puts """
+Alright, so you said #{likes} about liking me.
+You live in #{lives}. Not sure where that is.
+And you have a #{computer} computer. Nice.
+"""
