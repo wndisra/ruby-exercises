a4afcf7b6c0d51d0558623c04f612c05e2f753b2
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-12-17 19:44:44 +0700

    Add ex47 exercise

---
+++ ex47/lib/ex47/game.rb
@@ -0,0 +1,22 @@
+class Room
+
+  def initialize(name, description)
+    @name = name
+    @description = description
+    @paths = {}
+  end
+
+  # Define getters
+  attr_reader :name
+  attr_reader :paths
+  attr_reader :description
+
+  def go(direction)
+    return @paths[direction]
+  end
+
+  def add_paths(paths)
+    @paths.update(paths)
+  end
+
+end
